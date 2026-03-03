2aed45376e6a0b16d0309f2906c0f3b8095e42e0
Tinsel27 <karongpond@gmail.com>
2026-03-03 14:49:07 +0700

    bugfix

--- src/RenderObject/Block.java
+++ src/RenderObject/Block.java
@@ -1,8 +1,8 @@
 package RenderObject;
 import java.awt.image.*;
 public class Block extends GameObject {
-    public Block(Vector2D pos, Vector2D size) {
-        super(pos, size, new BufferImage());
+    public Block(Vector2D pos, Vector2D size,String ImageName) {
+        super(pos, size, ImageName);
     }
     
 }
--- src/RenderObject/GameObject.java
+++ src/RenderObject/GameObject.java
@@ -1,13 +1,20 @@
 package RenderObject;
 import java.awt.image.*;
 import java.io.*;
+import javax.imageio.ImageIO;
 abstract public class GameObject {    
     protected BufferedImage image;
     protected Vector2D postion;
-    
-    public GameObject(Vector2D pos, Vector2D size, file image) {
-        this.image = image;
+    protected Vector2D size;
+    public GameObject(Vector2D pos, Vector2D size, String ImageName) {
+        this.size=size;
         this.postion = pos;
+        
+        try{
+            this.image = ImageIO.read(new File(ImageName));
+        }catch (IOException e) {
+    e.printStackTrace();
+}
     }
     
     
