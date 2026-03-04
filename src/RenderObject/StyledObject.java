66056edd1b1a5944fcec9eda6c0b8e4a6bae4139
Tinsel27 <karongpond@gmail.com>
2026-03-04 18:08:26 +0700

    bugfix

--- src/RenderObject/StyledObject.java
+++ src/RenderObject/StyledObject.java
@@ -8,8 +8,8 @@ import javax.imageio.ImageIO;
 abstract public class StyledObject extends GameObject {
     protected BufferedImage image;
 
-    public StyledObject(String name, Vector2D pos, Vector2D size, String imageName) {
-        super(name, pos, size);
+    public StyledObject(String name, Vector2D pos, Vector2D size, String imageName,String type) {
+        super(name, pos, size,type);
         try {
             this.image = ImageIO.read(new File(imageName));
         } catch (IOException e) {
